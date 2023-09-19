169604dd2997eb5ec09cd160dc1aaab0e8fbf865
ngynmhieu <144605453+ngynmhieu@users.noreply.github.com>
2023-09-19 11:12:30 +0700

    Init STM32 Project

--- Exercise_8/Core/Src/main.c
+++ Exercise_8/Core/Src/main.c
@@ -175,39 +175,9 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   clearAllClock();
-  int hour= 0, min = 0, sec= -1; // sec = -1 because we want after the 1st second, the second hand point to 1
-  int round = 0 ; // show the program that sec or min have currently finished a round
    while (1)
    {
-	   sec++;
-	   if (sec-1 != hour && sec - 1 != min){ // check whether the current position of sec isn't hour or min to clear it
-		   clearNumberOnClock(sec-1);
-	   }
-	   if (sec >= 12){ // if sec number is over 12
-		   sec %=12;
-		   round = 1;
-	   }
-	   setNumberOnClock(sec);
-	   if (round ==1){ // when sec's just finished a round
-		   min ++; round = 0 ; // increase min 1 unit
-		   if (min -1 != hour && min -1 != sec){
-			   clearNumberOnClock(min -1);
-		   }
-		   if (min >=12){
-			   min%=12; round = 1;
-		   }
-		   setNumberOnClock(min);
-		   if (round ==1){ // when min's just finished a round
-			   hour ++; round = 0 ; // increase hour 1 unit
-			   if (hour -1 != min && hour -1 != sec){
-				   clearNumberOnClock(hour -1);
-			   }
-			   if (hour >=12 ) {
-				   hour %=12;
-			   }
-			   setNumberOnClock(hour);
-		   }
-	   }
+
 	   HAL_Delay (1000);
       /* USER CODE BEGIN 3 */
     }
