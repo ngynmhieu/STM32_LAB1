08d040cf0171b18e6100c395abd55067c9818160
ngynmhieu <144605453+ngynmhieu@users.noreply.github.com>
2023-09-19 17:54:09 +0700

    Init for Ex4

--- Ex4/Core/Src/main.c
+++ Ex4/Core/Src/main.c
@@ -9,7 +9,7 @@
   * <h2><center>&copy;  (c) 2023 .
   * All rights reserved.</center></h2>
   *
-  * This software component is licensed by ST under BSD 3-Clause license,
+  * This software component is licensed by  under BSD 3-Clause license,
   * the "License"; You may not use this file except in compliance with the
   * License. You may obtain a copy of the License at:
   *                        opensource.org/licenses/BSD-3-Clause
